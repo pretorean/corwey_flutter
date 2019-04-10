8d714a0d61d5d79b8a86778b3bf34d8598e7952e
Aleksey Radionov <pretorean@gmail.com>
2019-04-11 00:53:45 +0300

    отправка проверочного кода через сервис sms.ru

--- lib/SmsRuProvider.dart
+++ lib/SmsRuProvider.dart
@@ -1,11 +1,25 @@
 import 'package:meta/meta.dart';
+import 'package:http/http.dart' as http;
+import 'dart:math';
 
 class SmsRuProvider {
+  final String baseUrl =
+      'https://sms.ru/sms/send?api_id=65B77E45-C928-DB0C-5CFB-A76562537082';
+
   // отправляет сообщение и возвращает код, который отправлен
   Future<String> sendMessage({@required String phone}) async {
-    await Future.delayed(Duration(seconds: 1));
-    return _generateCode();
+    String code = _generateCode();
+    String requestText = '&to=$phone&msg=Код+подтверждения:+$code&json=1';
+
+    var response = await http.get(baseUrl + requestText);
+    if (response.statusCode == 200)
+      return code;
+    else {
+      throw Exception('Не удалось отправить СМС');
+    }
   }
 
-  String _generateCode() => '1111';
+  String _generateCode() {
+    return Random().nextInt(99999).toString().padLeft(5, '0');
+  }
 }
